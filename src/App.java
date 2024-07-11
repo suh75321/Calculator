f5e2287572787303748d1571ba566a80c36f189d
홍성욱 <suh75321@naver.com>
2024-07-11 17:18:39 +0900

    1레벨 두번째 char의 사용으로 사칙연산 입력

--- src/App.java
+++ src/App.java
@@ -7,6 +7,11 @@ public class App {
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt(); // Scanner를 사용하여 양의 정수를 입력받고 int 타입의 변수 num1에 저장합니다.
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0);// 사칙연산은 한글자니 char, 연산자는 operator를 써야한다.
+        //charAt(0) 메서드는 문자열에서 첫 번째 문자를 추출하는 역할을 한다.사용자가 "+" 문자열을 입력하면 sc.next()는 "+" 문자열을 반환하고, charAt(0)은 이 문자열의 첫 번째 문자인 '+'를 추출하고 이렇게 추출된 문자는 char 타입으로 처리된다.
+
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt(); // Scanner를 사용하여 양의 정수를 입력받고 int 타입의 변수 num2에 저장합니다.
     }
