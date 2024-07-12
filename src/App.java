2cbac25443aa1976a21d703064ed1009769748c1
홍성욱 <suh75321@naver.com>
2024-07-12 15:55:06 +0900

    1레벨 7번째 결과가 10개로 고정되지 않고 무한이 저장될 수 있도록 소스 코드를 수정. JCF 사용

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Objects;
 import java.util.Scanner;
@@ -6,8 +7,9 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 연산 결과를 저장할 배열을 선언하고 생성합니다.
-        int[] intArray = new int[10];
+        //jcf는 list, set, map이 있는데 list 사용
+        ArrayList<Integer> resultList = new ArrayList<>();//이건 크기가 정해지지 않아서 맘대로 추가, 삭제 가능
+//        int[] intArray = new int[10];
         // 배열의 마지막 인덱스를 저장하는 변수를 선언합니다.
         int index = 0;
 
@@ -56,24 +58,38 @@ public class App {
             System.out.println("결과: " + result);
 
 
-            // 연산 결과를 배열에 저장.
-            if (index < intArray.length) {//이걸로 인트어레이가 인덱스보다 많은지 검사
-                intArray[index] = result;//이 문장은 연산의 결과를 intArray의 index 위치에 저장 예를 들어, index가 0이면, result는 배열의 첫 번째 위치에 저장
-                index++;//저장하면 1증가
-                System.out.println("현재 저장된 결과의 수: " + index);//얼마나 저장했는지 보여줌
-            } else {
-                System.out.println("저장 공간이 차서 첫번째 결과값을 지우고 이번 결과 값을 넣었습니다.");//그냥 안내용
 
-                for (int i = 0; i < intArray.length - 1; i++) {
-                    intArray[i] = intArray[i + 1];//10개다 차면 -1하고 뒤에게 앞으로 한칸씩 이동
+
+//            if (index < intArray.length) {//이걸로 인트어레이가 인덱스보다 많은지 검사
+//                intArray[index] = result;//이 문장은 연산의 결과를 intArray의 index 위치에 저장 예를 들어, index가 0이면, result는 배열의 첫 번째 위치에 저장
+//                index++;//저장하면 1증가
+//                System.out.println("현재 저장된 결과의 수: " + index);//얼마나 저장했는지 보여줌
+//            } else {
+//                System.out.println("저장 공간이 차서 첫번째 결과값을 지우고 이번 결과 값을 넣었습니다.");//그냥 안내용
+//
+//                for (int i = 0; i < intArray.length - 1; i++) {
+//                    intArray[i] = intArray[i + 1];//10개다 차면 -1하고 뒤에게 앞으로 한칸씩 이동
+//                }
+//                intArray[intArray.length - 1] = result;//마지막 배열에 현재 결과값 입력
+//            }
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String text = sc.next();//밑에 있던걸 옮긴거뿐
+            if (Objects.equals(text, "remove")) {
+                if (!resultList.isEmpty()) {//결과가 비었는지 확인
+                    resultList.remove(0);//차있으면 첫번째(0)를 삭제
+                    System.out.println("첫번째 결과가 삭제되었습니다.");
+                } else {
+                    System.out.println("저장된 결과가 없습니다.");
                 }
-                intArray[intArray.length - 1] = result;//마지막 배열에 현재 결과값 입력
+            } else {
+                resultList.add(result);
+                System.out.println("현재 저장된 결과의 수: " + resultList.size());
             }
 
             //반복의 끝의 시작
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             /* exit을 입력 받으면 반복 종료 */
-            String text = sc.next();
             if (Objects.equals(text, "exit")) {
                 break;// 반복의 끝
             }
