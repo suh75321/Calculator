13a2453bf41a8f7139aa1378e48e31164596f815
홍성욱 <suh75321@naver.com>
2024-07-12 21:57:48 +0900

    2레벨 1번째 Calculator 작성

---
+++ src/Calculator.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Calculator {
+    private ArrayList<Integer> resultList = new ArrayList<>();
+
+    public int calculate(int num1, int num2, char operator) throws IllegalArgumentException {
+        int result;
+        if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 == 0) {
+                throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+            }
+            result = num1 / num2;
+        } else {
+            throw new IllegalArgumentException("잘못된 연산자입니다.");
+        }
+        resultList.add(result);
+        return result;
+    }
+
+
+}
+
